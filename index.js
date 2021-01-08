89ca04525669d2b8da97d13c33d59b121b9a623f
Tyler Bryant <tyler@nstrumenta.com>
2021-01-08 17:19:08 +0000

    remove mock data send

--- index.js
+++ index.js
@@ -186,28 +186,6 @@ SerialPort.list().then((ports) => {
 });
 
 var mockCanlogger = require("./mock-canlogger.json");
-
-let i = 0;
-setInterval(() => {
-  // console.log("testing BLE");
-  canloggerMessage = mockCanlogger[i++];
-  if (i == mockCanlogger.length) {
-    i = 0;
-  }
-  // console.log(canloggerMessage);
-  if (characteristic._updateValueCallback && canloggerMessage.data) {
-    dataArray = new Uint8Array(canloggerMessage.data.data);
-    var dataView = new DataView(dataArray.buffer);
-    var id = dataView.getUint32(8);
-    var dataLength = dataView.getUint8(12);
-    //if ([0x309, 0x156, 0x158, 0x1d0].includes(id)) {
-      if (characteristic._updateValueCallback) {
-        characteristic._updateValueCallback(dataArray);
-      }
-    //}
-  }
-}, 1000);
-
 //bleno
 var bleno = require("rpi-fix-bleno");
 
